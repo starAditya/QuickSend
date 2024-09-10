3e661a5a16fa6c46a4f3eee385c0eb98b3944dc8
starAditya <adityakesarwani82@gmail.com>
2024-09-10 15:05:11 +0530

    first commit

---
+++ server/routes/routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { uploadImage, getImage } from '../controller/image-controller.js';
+import upload from '../utils/upload.js';
+
+const router = express.Router();
+
+
+router.post('/upload', upload.single('file'), uploadImage);
+router.get('/file/:fileId', getImage);
+
+export default router;
